978d55203c42cc29442c7961e1f32bc44a10b4c0
Kirill <64526874+Kirillprogramm@users.noreply.github.com>
2022-07-18 19:20:37 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        Scanner scanner = new Scanner(System.in);
+
+        int goalStep = 1;
+        System.out.println("Ваше целевое значение шагов в день - " + goalStep);
+
+        String[] months = {"Январь","Февраль","Март","Апрель","Май", "Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+
+        StepTracker stepTracker = new StepTracker(months);
+
+        while(true) {
+            printMenu();
+            int userInput = scanner.nextInt();
+
+            if (userInput == 1) {
+                System.out.println("Выберите, за какой месяц вы хотите ввести шаги: ");
+                for (int month = 0; month < months.length; month++) {
+                    System.out.println((month + 1) + " - " + months[month]);
+                }
+
+                int monthDigits = scanner.nextInt();  // изменить ввод на цифры
+                String month = months[monthDigits - 1];
+
+                System.out.println("За какой день месяца " + month  + " вы хотите ввести шаги? Введите от 1 до 30:");
+                int day = scanner.nextInt();
+
+                System.out.println("Введите количество за шагов за " + day + " день месяца " + month + ":");
+                int step = scanner.nextInt();
+
+                stepTracker.addSteps(month, day, step);
+
+            } else if (userInput == 2) {
+                System.out.println("Выберите, за какой месяц вы хотите получить статистику: ");
+                for (int month = 0; month < months.length; month++) {
+                    System.out.println((month + 1 ) + " - " + months[month]);
+                }
+
+                int monthInt = scanner.nextInt(); // изменить ввод на цифры
+                String month = months[monthInt - 1];
+
+                stepTracker.goStepsInDay(month);
+                stepTracker.sumStepMonth(month);
+                stepTracker.maxStepMonth(month);
+                stepTracker.averageStepMonth(month);
+                stepTracker.pathsPassed(month);
+                stepTracker.energySpent(month);
+                stepTracker.bestSeries(goalStep, month);
+
+            } else if (userInput == 3) {
+
+                goalStep = stepTracker.newGoalStep();
+
+                System.out.println("Целевое количество шагов изменено.");
+                System.out.println("Ваша новая цель - " + goalStep + " шагов в день!");
+
+            } else if (userInput == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1. Ввести количество шагов за определённый день месяца");
+        System.out.println("2. Напечатать статистику за определённый месяц");
+        System.out.println("3. Изменить цель по количеству шагов в день");
+        System.out.println("0. Выход");
     }
 }
---
+++ src/StepTracker.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class StepTracker {
+    Converter converter = new Converter();
+    Scanner scanner = new Scanner(System.in);
+
+    HashMap<String, ArrayList<Integer>> monthOfDay;
+    ArrayList<Integer> daysInMonth;
+    int[] days = new int[30];
+
+    StepTracker(String[] months) {
+        monthOfDay = new HashMap<>();
+        for (String month : months) {
+            monthOfDay.put(month, daysInMonth);
+        }
+        daysInMonth = new ArrayList<>();
+        for (int i : days) {
+            daysInMonth.add(i);
+        }
+    }
+
+    void addSteps(String month, int day, int step) {
+        if (step >= 0) {
+            daysInMonth.set((day - 1), step);
+            monthOfDay.put(month, daysInMonth);
+            System.out.println("Шаги сохранены.");
+        } else {
+            System.out.println("Количество шагов не может быть отрицательным.");
+        }
+    }
+
+    void goStepsInDay(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        for (int i = 0 ; i < stepsPerDay.size(); i++) {
+            System.out.println((i + 1) + " день: " + stepsPerDay.get(i));
+        }
+    }
+
+    void sumStepMonth(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        int sumStepMonth = 0;
+        for (Integer sumStep : stepsPerDay) {
+            sumStepMonth += sumStep;
+        }
+        System.out.println("За месяц вы прошли "+ sumStepMonth + " шагов." );
+    }
+
+    void maxStepMonth(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        int maxStepMonth = 0;
+        for (Integer max : stepsPerDay) {
+            if (max > maxStepMonth) {
+                maxStepMonth = max;
+            }
+        }
+        System.out.println("Максимальное количество шагов за " + month + " - "+ maxStepMonth);
+    }
+
+
+    void averageStepMonth(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        int sumStepMonth = 0;
+        for (Integer sumStep : stepsPerDay) {
+            sumStepMonth += sumStep;
+        }
+        int averageStepMonth = sumStepMonth / stepsPerDay.size();
+        System.out.println("Среднее количество шагов за " + month + " - " + averageStepMonth);
+    }
+
+    void pathsPassed(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        int sumStepMonth = 0;
+        for (Integer sumStep : stepsPerDay) {
+            sumStepMonth += sumStep;
+        }
+        System.out.println("Пройденная дистанция за " + month + " - " + converter.distanceInKilometres(sumStepMonth)+ " километров");
+    }
+
+    void energySpent(String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        int sumStepMonth = 0;
+        for (Integer sumStep : stepsPerDay) {
+            sumStepMonth += sumStep;
+        }
+        System.out.println("Потрачено каллорий за " + month + " - " + converter.cCal(sumStepMonth) + " килокаллорий");
+    }
+
+    void bestSeries(int goalStep, String month) {
+        ArrayList<Integer> stepsPerDay = monthOfDay.get(month);
+        ArrayList<Integer> listBestSeries = new ArrayList<>();
+
+        int dayRow = 0;
+        for (int i = 0; i < stepsPerDay.size(); i++) {
+            if (stepsPerDay.get(i) > goalStep) {
+                if (i - (i - 1) == 1) {
+                    dayRow++; // плюсую 1 к счетчику серии
+                    System.out.println("серия - " + dayRow + " дней.");
+                }
+            }
+            listBestSeries.add(dayRow);
+        }
+
+        int bestSeries = 0;
+        for (Integer row : listBestSeries) {
+            if (bestSeries < row) {
+                bestSeries = row;
+            }
+
+        }
+        System.out.println("Лучшая серия - " + (bestSeries) + " дней.");
+    }
+
+    int newGoalStep(){
+        System.out.println("Введите новое целевое значение шагов в день");
+
+        return scanner.nextInt();
+    }
+
+}
